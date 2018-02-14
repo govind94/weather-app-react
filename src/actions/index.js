f591a470364331d285e2849ede03bcbe23f45beb
Govindan Vedanarayanan <vamsivedanarayanan@gmail.com>
2018-02-14 01:30:40 -0500

    https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = "1aeb5bcc85eb6874d22f7c238ea5a577";
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
 

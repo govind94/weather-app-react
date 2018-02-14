381de4871e6c60e5801625beb5a69fb5c6bbf71d
Govindan Vedanarayanan <vamsivedanarayanan@gmail.com>
2018-02-14 01:40:55 -0500

    Add google key

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,5 +1,3 @@
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDi91PrtWMRoFzkRyLOFBgohm1sVEp8N1s&callback=initMap" type="text/javascript"></script>
-
 import React, { Component } from "react";
 
 class GoogleMap extends Component {
